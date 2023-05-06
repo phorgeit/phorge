phorgeit/phorge

commit b56d86e48d0f63028cb5739c179d61ece2a24bb1

Fix numerous PHP 8.1 "strlen(null)" exceptions preventing homepage to display
Summary:
Fix numerous PHP 8.1 RuntimeExceptions caused by the deprecation of strlen(null).

The strlen() was used in Phabricator to check if a generic value was a non-empty string.
For this reason, Phorge adopts phutil_nonempty_string() that checks that.

Note: this may highlight other absurd input values that might be worth correcting
instead of just ignoring. If your phutil_nonempty_string() throws an exception, just
report it to Phorge to evaluate and fix together that specific corner case.

Closes T15264

Test Plan: Phorge homepage is displayed on PHP 8.1 after applying these changes

Reviewers: O1 Blessed Committers, valerio.bozzolan

Reviewed By: O1 Blessed Committers, valerio.bozzolan

Subscribers: speck, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Maniphest Tasks: T15264

Differential Revision: https://we.phorge.it/D25132

src/view/phui/PHUIObjectItemView.php
@@ -659,7 +659,8 @@ final class PHUIObjectItemView extends AphrontTagView {
         $this->getImageIcon());
     }
 
-    if ($image && (strlen($this->href) || strlen($this->imageHref))) {
+    if ($image && (phutil_nonempty_string($this->href) ||
+        phutil_nonempty_string($this->imageHref))) {
       $image_href = ($this->imageHref) ? $this->imageHref : $this->href;
       $image = phutil_tag(
         'a',

src/applications/config/check/PhabricatorDaemonsSetupCheck.php
@@ -53,7 +53,7 @@ final class PhabricatorDaemonsSetupCheck extends PhabricatorSetupCheck {
     }
 
     $expect_user = PhabricatorEnv::getEnvConfig('phd.user');
-    if (strlen($expect_user)) {
+    if (phutil_nonempty_string($expect_user)) {
 
       try {
         $all_daemons = id(new PhabricatorDaemonLogQuery())

src/applications/files/view/PhabricatorGlobalUploadTargetView.php
@@ -67,7 +67,7 @@ final class PhabricatorGlobalUploadTargetView extends AphrontView {
     require_celerity_resource('global-drag-and-drop-css');
 
     $hint_text = $this->getHintText();
-    if (!strlen($hint_text)) {
+    if (!phutil_nonempty_string($hint_text)) {
       $hint_text = "\xE2\x87\xAA ".pht('Drop Files to Upload');
     }
 

src/applications/home/menuitem/PhabricatorHomeLauncherProfileMenuItem.php
@@ -31,7 +31,7 @@ final class PhabricatorHomeLauncherProfileMenuItem
     PhabricatorProfileMenuItemConfiguration $config) {
     $name = $config->getMenuItemProperty('name');
 
-    if (strlen($name)) {
+    if (phutil_nonempty_string($name)) {
       return $name;
     }
 

src/applications/search/engine/PhabricatorProfileMenuEngine.php
@@ -1308,7 +1308,7 @@ abstract class PhabricatorProfileMenuEngine extends Phobject {
     // render the default view instead.
 
     $selected_view = null;
-    if (strlen($item_id)) {
+    if (phutil_nonempty_string($item_id)) {
       $item_views = $view_list->getViewsWithItemIdentifier($item_id);
       if ($item_views) {
         $selected_view = head($item_views);

src/applications/search/engine/PhabricatorProfileMenuItemView.php
@@ -140,7 +140,7 @@ final class PhabricatorProfileMenuItemView
       ->setName($this->getName());
 
     $uri = $this->getURI();
-    if (strlen($uri)) {
+    if (phutil_nonempty_string($uri)) {
       if ($this->getIsExternalLink()) {
         if (!PhabricatorEnv::isValidURIForLink($uri)) {
           $uri = '#';
@@ -176,7 +176,7 @@ final class PhabricatorProfileMenuItemView
     }
 
     $tooltip = $this->getTooltip();
-    if (strlen($tooltip)) {
+    if (phutil_nonempty_string($tooltip)) {
       $view->setTooltip($tooltip);
     }
 

src/applications/search/menuitem/PhabricatorDashboardProfileMenuItem.php
@@ -117,7 +117,7 @@ final class PhabricatorDashboardProfileMenuItem
       return pht('Archived Dashboard');
     }
 
-    if (strlen($this->getName($config))) {
+    if (phutil_nonempty_string($this->getName($config))) {
       return $this->getName($config);
     } else {
       return $dashboard->getName();

src/applications/search/menuitem/PhabricatorEditEngineProfileMenuItem.php
@@ -71,7 +71,7 @@ final class PhabricatorEditEngineProfileMenuItem
     if (!$form) {
       return pht('(Restricted/Invalid Form)');
     }
-    if (strlen($this->getName($config))) {
+    if (phutil_nonempty_string($this->getName($config))) {
       return $this->getName($config);
     } else {
       return $form->getName();

commit e9e2c954132cf9d548e0ac9eb7f75c88f8308c8d

Fix regression in PHUIObjectItemView.php:662: allow PhutilURI and other stringlike
Summary:
This fixes this specific exception that can happen with whatever PHP version in some pages:

    Call to phutil_nonempty_string() expected null or a string, got: PhutilURI from PHUIObjectItemView.php:662

The regression was introduced since:

b56d86e48d0f63028cb5739c179d61ece2a24bb1

The problem is caused by the fact that we are trying to introduce very strict checks, and sometime we are too much strict.

In this specific case we use `phutil_nonempty_stringlike()` since it also allows objects with a `__toString()` method.

In order not to leave these cases to chance, we have added a log line, which can be removed in the future.

If you see this log line, report it in the respective Task. Thank you!

Closes T15306
Ref T15316

Test Plan:
1. {nav Dashboard > Add a Panel}: no crash
2. {nav Diffusion repo > README}: no crash
3. {nav Herald > Create}: no crash

Reviewers: O1 Blessed Committers, avivey

Reviewed By: O1 Blessed Committers, avivey

Subscribers: avivey, aklapper, speck, tobiaswiese, Matthew, Cigaryno

Maniphest Tasks: T15306, T15316

Differential Revision: https://we.phorge.it/D25157

src/view/phui/PHUIObjectItemView.php
@@ -83,11 +83,29 @@ final class PHUIObjectItemView extends AphrontTagView {
     return $this->object;
   }
 
+  /**
+   * Set the href attribute
+   *
+   * @param string|PhutilURI|null $href
+   * @return self
+   */
   public function setHref($href) {
+
+    // We have not a very clear idea about what this method should receive
+    // So, let's log alien stuff for some time
+    // https://we.phorge.it/T15316
+    self::requireValidHref($href, 'href');
+
     $this->href = $href;
     return $this;
   }
 
+  /**
+   * Get the href attribute
+   *
+   * @see PHUIObjectItemView::setHref()
+   * @return string|PhutilURI|null
+   */
   public function getHref() {
     return $this->href;
   }
@@ -136,7 +154,19 @@ final class PHUIObjectItemView extends AphrontTagView {
     return $this;
   }
 
+  /**
+   * Set the image href attribute
+   *
+   * @param string|PhutilURI|null $image_href
+   * @return self
+   */
   public function setImageHref($image_href) {
+
+    // We have not a very clear idea about what this method should receive
+    // So, let's log alien stuff for some time
+    // https://we.phorge.it/T15316
+    self::requireValidHref($image_href, 'image_href');
+
     $this->imageHref = $image_href;
     return $this;
   }
@@ -659,8 +689,8 @@ final class PHUIObjectItemView extends AphrontTagView {
         $this->getImageIcon());
     }
 
-    if ($image && (phutil_nonempty_string($this->href) ||
-        phutil_nonempty_string($this->imageHref))) {
+    if ($image && (phutil_nonempty_stringlike($this->href) ||
+        phutil_nonempty_stringlike($this->imageHref))) {
       $image_href = ($this->imageHref) ? $this->imageHref : $this->href;
       $image = phutil_tag(
         'a',
@@ -897,6 +927,32 @@ final class PHUIObjectItemView extends AphrontTagView {
     }
 
     return javelin_tag('span', $options, '');
+  }
+
+
+  /**
+   * Receive a href attribute and check if it has expected values
+   *
+   * TODO: Feel free to remove after 2023, if no more new reports arrive.
+   *
+   * https://we.phorge.it/T15316
+   *
+   * @param mixed  $href Value to be checked
+   * @param string $variable_name Human reference
+   */
+  private static function requireValidHref($href, $variable_name) {
+
+    // We have not a very clear idea about what a "href" should be
+    if (is_object($href) && !($href instanceof PhutilURI)) {
+
+      // We log stuff with a kind stack trace
+      phlog(new Exception(pht(
+        'The variable %s received an unexpected type: %s. '.
+        'Please share this stack trace as comment in Task %s',
+        $variable_name,
+        get_class($href),
+        'https://we.phorge.it/T15316')));
+    }
   }
 
 }