aa8baf1c8b59c81b9d5549ee4ad8f53aa1415d1a
Andre Klapper <a9016009@gmx.de>
2023-05-08 14:34:01 +0200

    Fix PHP 8.1 "strlen(null)" exception which blocks rendering a project's page when Reports prototype enabled
    Summary:
    `strlen()` was used in Phabricator to check if a generic value is a non-empty string.
    This behavior is deprecated since PHP 8.1. Phorge adopts `phutil_nonempty_string()` as a replacement.
    
    Note: this may highlight other absurd input values that might be worth correcting
    instead of just ignoring. If phutil_nonempty_string() throws an exception in your
    instance, report it to Phorge to evaluate and fix that specific corner case.
    
    This is exactly the same case as D25140.
    
    Closes T15360
    
    Test Plan: Applied this change and `/project/view/1/` rendered the workboard in the web browser as expected.
    
    Reviewers: O1 Blessed Committers, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, valerio.bozzolan
    
    Subscribers: speck, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15360
    
    Differential Revision: https://we.phorge.it/D25199

--- src/applications/project/menuitem/PhabricatorProjectReportsProfileMenuItem.php
+++ src/applications/project/menuitem/PhabricatorProjectReportsProfileMenuItem.php
@@ -46,7 +46,7 @@ final class PhabricatorProjectReportsProfileMenuItem
     PhabricatorProfileMenuItemConfiguration $config) {
     $name = $config->getMenuItemProperty('name');
 
-    if (strlen($name)) {
+    if (phutil_nonempty_string($name)) {
       return $name;
     }
 
