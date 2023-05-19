phorgeit/phorge

commit 562d36ef5f9e83dba5e3ca0244f9c4af9e1f7b2b

Fix PHP 8.1 "strlen(null)" exceptions to render the Account Creation page
Summary:
Fix numerous PHP 8.1 "strlen(null)" exceptions which block rendering the initial Account
Creation page in a fresh Phorge installation.

The strlen() was used in Phabricator to check if a generic value was a non-empty string.
For this reason, Phorge adopts phutil_nonempty_string() that checks that.

Note: this may highlight other absurd input values that might be worth correcting
instead of just ignoring. If your phutil_nonempty_string() throws an exception, just
report it to Phorge to evaluate and fix together that specific corner case.

Closes T15279

Test Plan: After these code changes the account creation page got displayed (though without CSS and JS).

Reviewers: O1 Blessed Committers, valerio.bozzolan

Reviewed By: O1 Blessed Committers, valerio.bozzolan

Subscribers: avivey, speck, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Maniphest Tasks: T15279

Differential Revision: https://we.phorge.it/D25137

src/view/form/control/AphrontFormControl.php
@@ -172,7 +172,7 @@ abstract class AphrontFormControl extends AphrontView {
       $this->renderInput());
 
     $error = null;
-    if (strlen($this->getError())) {
+    if ($this->getError()) {
       $error = $this->getError();
       if ($error === true) {
         $error = phutil_tag(
@@ -187,7 +187,7 @@ abstract class AphrontFormControl extends AphrontView {
       }
     }
 
-    if (strlen($this->getLabel())) {
+    if (phutil_nonempty_string($this->getLabel())) {
       $label = phutil_tag(
         'label',
         array(
@@ -203,7 +203,7 @@ abstract class AphrontFormControl extends AphrontView {
       $custom_class .= ' aphront-form-control-nolabel';
     }
 
-    if (strlen($this->getCaption())) {
+    if (phutil_nonempty_string($this->getCaption())) {
       $caption = phutil_tag(
         'div',
         array('class' => 'aphront-form-caption'),

src/applications/auth/controller/PhabricatorAuthRegisterController.php
@@ -18,7 +18,7 @@ final class PhabricatorAuthRegisterController
     $invite = $this->loadInvite();
 
     $is_setup = false;
-    if (strlen($account_key)) {
+    if (phutil_nonempty_string($account_key)) {
       $result = $this->loadAccountForRegistrationOrLinking($account_key);
       list($account, $provider, $response) = $result;
       $is_default = false;
@@ -244,9 +244,9 @@ final class PhabricatorAuthRegisterController
 
     $require_real_name = PhabricatorEnv::getEnvConfig('user.require-real-name');
 
-    $e_username = strlen($value_username) ? null : true;
+    $e_username = phutil_nonempty_string($value_username) ? null : true;
     $e_realname = $require_real_name ? true : null;
-    $e_email = strlen($value_email) ? null : true;
+    $e_email = phutil_nonempty_string($value_email) ? null : true;
     $e_password = true;
     $e_captcha = true;
 

src/applications/base/controller/PhabricatorController.php
@@ -74,7 +74,7 @@ abstract class PhabricatorController extends AphrontController {
       $session_engine = new PhabricatorAuthSessionEngine();
 
       $phsid = $request->getCookie(PhabricatorCookies::COOKIE_SESSION);
-      if (strlen($phsid)) {
+      if (phutil_nonempty_string($phsid)) {
         $session_user = $session_engine->loadUserForSession(
           PhabricatorAuthSession::TYPE_WEB,
           $phsid);

commit 976a21f6584bda13289cee12599d29cb3126dbd4

AphrontFormControl: fix regression for some specific Captions
Summary:
Fix a specific problem when visiting some specific pages like this one:

    /auth/config/edit/?provider=PhabricatorPhabricatorAuthProvider:

Regression introduced in:

562d36ef5f9e83dba5e3ca0244f9c4af9e1f7b2b

Stack trace:

    [Fri May 19 14:23:35.506028 2023] [php7:notice] [pid 20439] [client 127.0.0.1:39692] [2023-05-19 14:23:35] EXCEPTION: (InvalidArgumentException) Call to phutil_nonempty_string() expected null or a string, got: PhutilSafeHTML. at [<arcanist>/src/utils/utils.php:2127]
    [Fri May 19 14:23:35.506647 2023] [php7:notice] [pid 20439] [client 127.0.0.1:39692] arcanist(head=arcpatch-D25049, ref.master=c14785c3795c, ref.arcpatch-D25049=1b6412c24640), phorge(head=arcpatch-D25216_1, ref.master=2df7ea13a387, ref.arcpatch-D25216_1=02b40a9e25eb)
    [Fri May 19 14:23:35.506661 2023] [php7:notice] [pid 20439] [client 127.0.0.1:39692]   #0 <#2> phutil_nonempty_string(PhutilSafeHTML) called at [<phorge>/src/view/form/control/AphrontFormControl.php:206]
    [Fri May 19 14:23:35.506665 2023] [php7:notice] [pid 20439] [client 127.0.0.1:39692]   #1 <#2> phutil_tag(string, array, array) called at [<phorge>/src/view/form/PHUIFormLayoutView.php:54]
    [Fri May 19 14:23:35.506667 2023] [php7:notice] [pid 20439] [client 127.0.0.1:39692]   #2 <#2> PHUIFormLayoutView::render() called at [<phorge>/src/view/form/AphrontFormView.php:160]

We keep 100% backward compatibility, reproducing the implicit cast happening automatically before PHP 8.1

It has also been simplified the non-empty check since that is possible after casting to string.

Closes T15404

Test Plan:
- Visit the page mentioned in the commit summary
- no longer explodes
- you still don't see empty Captions
- you still see populated Captions
- you still see HTML rendered for populated Captions

Reviewers: O1 Blessed Committers, avivey

Reviewed By: O1 Blessed Committers, avivey

Subscribers: avivey, speck, tobiaswiese, Matthew, Cigaryno

src/view/form/control/AphrontFormControl.php
@@ -56,11 +56,22 @@ abstract class AphrontFormControl extends AphrontView {
     return $this->label;
   }
 
+  /**
+   * Set the Caption
+   * The Caption shows a tip usually nearby the related input field.
+   * @param string|PhutilSafeHTML|null
+   * @return self
+   */
   public function setCaption($caption) {
     $this->caption = $caption;
     return $this;
   }
 
+  /**
+   * Get the Caption
+   * The Caption shows a tip usually nearby the related input field.
+   * @return string|PhutilSafeHTML|null
+   */
   public function getCaption() {
     return $this->caption;
   }
@@ -203,7 +214,11 @@ abstract class AphrontFormControl extends AphrontView {
       $custom_class .= ' aphront-form-control-nolabel';
     }
 
-    if (phutil_nonempty_string($this->getCaption())) {
+    // The Caption can be stuff like PhutilSafeHTML and other objects that
+    // can be casted to a string. After this cast we have never null.
+    $has_caption = phutil_string_cast($this->getCaption()) !== '';
+
+    if ($has_caption) {
       $caption = phutil_tag(
         'div',
         array('class' => 'aphront-form-caption'),