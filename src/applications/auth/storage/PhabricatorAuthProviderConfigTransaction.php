b445e1d80df90f7fb1a2955fde818e72359970b7
Mark Jervelund <mark@jervelund.com>
2024-02-12 11:51:24 +0100

    Fix config transaction rendering error on auth page by allowing PhutilSafeHTML
    Summary: Closes T15706
    
    Test Plan: Checked that the page worked as expected with changes implemented
    
    Reviewers: O1 Blessed Committers, speck, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, speck, valerio.bozzolan
    
    Subscribers: sirocyl, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15706
    
    Differential Revision: https://we.phorge.it/D25512

--- src/applications/auth/storage/PhabricatorAuthProviderConfigTransaction.php
+++ src/applications/auth/storage/PhabricatorAuthProviderConfigTransaction.php
@@ -150,7 +150,7 @@ final class PhabricatorAuthProviderConfigTransaction
         $provider = $this->getProvider();
         if ($provider) {
           $title = $provider->renderConfigPropertyTransactionTitle($this);
-          if (phutil_nonempty_string($title)) {
+          if (phutil_nonempty_stringlike($title)) {
             return $title;
           }
         }
