8a3b70aa7d4c3376dabf5dfd5d3a0df68998711c
Andre Klapper <a9016009@gmx.de>
2024-05-13 18:02:55 +0200

    Run ./bin/celerity map after previous commit
    Summary: Followup to rP67b7181f522673b05435083ce28106bdfa320e18
    
    Test Plan: Run `./bin/celerity map` and see changes due to my previous commit.
    
    Reviewers: O1 Blessed Committers, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, valerio.bozzolan
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D25637

--- resources/celerity/map.php
+++ resources/celerity/map.php
@@ -396,7 +396,7 @@ return array(
     'rsrc/js/application/diffusion/behavior-pull-lastmodified.js' => 'c715c123',
     'rsrc/js/application/doorkeeper/behavior-doorkeeper-tag.js' => '6a85bc5a',
     'rsrc/js/application/drydock/drydock-live-operation-status.js' => '47a0728b',
-    'rsrc/js/application/fact/Chart.js' => '52e3ff03',
+    'rsrc/js/application/fact/Chart.js' => '0076c427',
     'rsrc/js/application/fact/ChartCurtainView.js' => '86954222',
     'rsrc/js/application/fact/ChartFunctionLabel.js' => '81de1dab',
     'rsrc/js/application/files/behavior-document-engine.js' => '243d6c22',
@@ -702,7 +702,7 @@ return array(
     'javelin-behavior-user-menu' => '60cd9241',
     'javelin-behavior-view-placeholder' => 'a9942052',
     'javelin-behavior-workflow' => '9623adc1',
-    'javelin-chart' => '52e3ff03',
+    'javelin-chart' => '0076c427',
     'javelin-chart-curtain-view' => '86954222',
     'javelin-chart-function-label' => '81de1dab',
     'javelin-color' => '78f811c9',
@@ -918,6 +918,12 @@ return array(
     'unhandled-exception-css' => '9ecfc00d',
   ),
   'requires' => array(
+    '0076c427' => array(
+      'phui-chart-css',
+      'd3',
+      'javelin-chart-curtain-view',
+      'javelin-chart-function-label',
+    ),
     '0116d3e8' => array(
       'javelin-behavior',
       'javelin-dom',
@@ -1418,12 +1424,6 @@ return array(
       'javelin-install',
       'javelin-dom',
       'javelin-fx',
-    ),
-    '52e3ff03' => array(
-      'phui-chart-css',
-      'd3',
-      'javelin-chart-curtain-view',
-      'javelin-chart-function-label',
     ),
     '541f81c3' => array(
       'javelin-install',
