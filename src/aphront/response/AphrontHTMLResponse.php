1f19cbf7486a5207978a11176998e39efb94eb1a
Andre Klapper <a9016009@gmx.de>
2024-06-18 12:34:52 +0200

    Set "preconnect" HTTP header when "security.alternate-file-domain" is set
    Summary:
    When a CDN or alternate file domain is configured, reduce perceived latency by resolving DNS and establishing a connection.
    
    See https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/rel/preconnect and https://we.phorge.it/book/phorge/article/configuring_file_domain/
    
    Closes T15859
    
    Test Plan:
    1. Set http://phorge.localhost/config/edit/security.alternate-file-domain/ via `./bin/config set "security.alternate-file-domain" "https://whatever.example.com/"`
    2. Open the "Network" tab in your web browser's "Developer Tools" and go to http://phorge.localhost/
    3. Check under "Headers" that "Response Headers" includes the new header `Link: <https://whatever.example.com/>; rel="preconnect"`.
    
    Reviewers: O1 Blessed Committers, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, valerio.bozzolan
    
    Subscribers: avivey, speck, l2dy, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15859
    
    Differential Revision: https://we.phorge.it/D25694

--- src/aphront/response/AphrontHTMLResponse.php
+++ src/aphront/response/AphrontHTMLResponse.php
@@ -6,6 +6,11 @@ abstract class AphrontHTMLResponse extends AphrontResponse {
     $headers = array(
       array('Content-Type', 'text/html; charset=UTF-8'),
     );
+    $cdn = PhabricatorEnv::getEnvConfig('security.alternate-file-domain');
+    if ($cdn) {
+      $headers[] = array('Link', '<'.$cdn.'>; rel="preconnect"');
+    }
+
     $headers = array_merge(parent::getHeaders(), $headers);
     return $headers;
   }
