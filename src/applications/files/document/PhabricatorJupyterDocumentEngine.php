b08c7c761c1078658da02ba68fcb4b0f67912438
Andre Klapper <a9016009@gmx.de>
2024-10-21 20:07:17 +0200

    Fix PHP 8.1 "strlen(null)" exception rendering JSON file as Jupyter
    Summary:
    `strlen()` was used in Phabricator to check if a generic value is a non-empty string.
    This behavior is deprecated since PHP 8.1. Phorge adopts `phutil_nonempty_string()` as a replacement.
    
    Note: this may highlight other absurd input values that might be worth correcting
    instead of just ignoring. If phutil_nonempty_string() throws an exception in your
    instance, report it to Phorge to evaluate and fix that specific corner case.
    
    ```
    ERROR 8192: strlen(): Passing null to parameter #1 ($string) of type string is deprecated at [/var/www/html/phorge/phorge/src/applications/files/document/PhabricatorJupyterDocumentEngine.php:326]
    ```
    
    Closes T15951
    
    Test Plan: See T15951
    
    Reviewers: O1 Blessed Committers, speck
    
    Reviewed By: O1 Blessed Committers, speck
    
    Subscribers: speck, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15951
    
    Differential Revision: https://we.phorge.it/D25830

--- src/applications/files/document/PhabricatorJupyterDocumentEngine.php
+++ src/applications/files/document/PhabricatorJupyterDocumentEngine.php
@@ -323,7 +323,7 @@ final class PhabricatorJupyterDocumentEngine
     }
 
     $nbformat = idx($data, 'nbformat');
-    if (!strlen($nbformat)) {
+    if (!phutil_nonempty_string($nbformat)) {
       throw new Exception(
         pht(
           'This document is missing an "nbformat" field. Jupyter notebooks '.
