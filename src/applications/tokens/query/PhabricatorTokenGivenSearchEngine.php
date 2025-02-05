75267e24e7e10bd6f8342d9750d74ec4b61de6cf
Taavi Väänänen <hi@taavi.wtf>
2025-02-05 19:24:33 +0200

    Convert tokens given to use SearchEngine
    Summary:
    Now the page /token/given/ allows to sort tokens by newest (default) and oldest.
    The default sort is unchanged.
    
    The given tokens are also now easily usable into any Dashboard.
    
    This introduces creative space to add future filters.
    
    refs T15988
    
    Test Plan:
    Tested that /token/ still renders fine.
    
    Order by Creation (Newest First): it works.
    
    Order by Creation (Oldest First): it works.
    
    Activate the DarkConsole's top bar and open the tab Services to inspect the generated queries, that are like this, and not anything alien,
    accordingly to the current "order by":
    
        SELECT * FROM `token_given`      ORDER BY `id` DESC LIMIT 101
        SELECT * FROM `token_given`      ORDER BY `id` ASC LIMIT 101
    
    Test the page /token/given/ as logged-out: it the Tokens app is configured as public, it still works like before.
    
    From the page /token/given/ order by "Creation (oldest first)", use {nav Use Results > Add Dashboard} and see that it works.
    
    From the same Dashboard: {nav Create Panel > Query Panel} and select "Tokens Given" with Limit=1 and title "Most Recent Tokenzzz" and see that it works as expected.
    
    Visit the Leader Board page at /token/leaders/: it still works like before.
    
    Reviewers: O1 Blessed Committers, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, valerio.bozzolan
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15988
    
    Differential Revision: https://we.phorge.it/D25863

---
+++ src/applications/tokens/query/PhabricatorTokenGivenSearchEngine.php
@@ -0,0 +1,96 @@
+<?php
+
+final class PhabricatorTokenGivenSearchEngine
+  extends PhabricatorApplicationSearchEngine {
+
+  public function getResultTypeDescription() {
+    return pht('Tokens Given');
+  }
+
+  public function getApplicationClassName() {
+    return PhabricatorTokensApplication::class;
+  }
+
+  public function newQuery() {
+    return new PhabricatorTokenGivenQuery();
+  }
+
+  protected function buildCustomSearchFields() {
+    return array();
+  }
+
+  protected function buildQueryFromParameters(array $map) {
+    return $this->newQuery();
+  }
+
+  protected function getRequiredHandlePHIDsForResultList(
+    array $tokens_given,
+    PhabricatorSavedQuery $query) {
+    $object_phids = mpull($tokens_given, 'getObjectPHID');
+    $viewer_phids = mpull($tokens_given, 'getAuthorPHID');
+    return array_merge($object_phids, $viewer_phids);
+  }
+
+  protected function renderResultList(
+    array $tokens_given,
+    PhabricatorSavedQuery $saved,
+    array $handles) {
+    $viewer = $this->requireViewer();
+
+    $tokens = array();
+    if ($tokens_given) {
+      $token_phids = mpull($tokens_given, 'getTokenPHID');
+      $tokens = id(new PhabricatorTokenQuery())
+        ->setViewer($viewer)
+        ->withPHIDs($token_phids)
+        ->execute();
+      $tokens = mpull($tokens, null, 'getPHID');
+    }
+
+    $list = new PHUIObjectItemListView();
+    foreach ($tokens_given as $token_given) {
+      $handle = $handles[$token_given->getObjectPHID()];
+      $token = idx($tokens, $token_given->getTokenPHID());
+
+      $item = id(new PHUIObjectItemView());
+      $item->setHeader($handle->getFullName());
+      $item->setHref($handle->getURI());
+
+      $item->addAttribute($token->renderIcon());
+
+      $item->addAttribute(
+        pht(
+          'Given by %s on %s',
+          $handles[$token_given->getAuthorPHID()]->renderLink(),
+          phabricator_date($token_given->getDateCreated(), $viewer)));
+
+      $list->addItem($item);
+    }
+
+    return id(new PhabricatorApplicationSearchResultView())
+      ->setObjectList($list);
+  }
+
+  protected function getBuiltinQueryNames() {
+    $names = array();
+    $names['all'] = pht('All Tokens Given');
+
+    return $names;
+  }
+
+  public function buildSavedQueryFromBuiltin($query_key) {
+    $query = $this->newSavedQuery();
+    $query->setQueryKey($query_key);
+
+    switch ($query_key) {
+      case 'all':
+        return $query;
+    }
+
+    return parent::buildSavedQueryFromBuiltin($query_key);
+  }
+
+  protected function getURI($path) {
+    return '/token/given/'.$path;
+  }
+}
