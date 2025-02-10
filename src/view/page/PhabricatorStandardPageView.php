372316c998362e3530c0e591fe0869e6a7e62af6
Andre Klapper <a9016009@gmx.de>
2025-02-10 20:29:13 +0100

    Fix PHP 8.1 "preg_match(null)" exception for missing User-Agent HTTP Header
    Summary:
    Passing `null` to `preg_match()` is deprecated behavior since PHP 8.1.
    Some clients do not pass a `User-Agent` HTTP header so `$agent` is null.
    Thus only call `preg_match()` when `$agent` is set.
    
    ```
    ERROR 8192: preg_match(): preg_match(): Passing null to parameter #2 ($subject) of type string is deprecated
    #0 preg_match(string, NULL) called at [<phorge>/src/view/page/PhabricatorStandardPageView.php:629]
    ```
    
    Closes T15829
    
    Test Plan: Access a task page etc in a browser which does not set a `User-Agent` HTTP Header string.
    
    Reviewers: O1 Blessed Committers, mainframe98, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, mainframe98, valerio.bozzolan
    
    Subscribers: mainframe98, slip, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T15829
    
    Differential Revision: https://we.phorge.it/D25868

--- src/view/page/PhabricatorStandardPageView.php
+++ src/view/page/PhabricatorStandardPageView.php
@@ -626,20 +626,25 @@ final class PhabricatorStandardPageView extends PhabricatorBarePageView
     // Try to guess the device resolution based on UA strings to avoid a flash
     // of incorrectly-styled content.
     $device_guess = 'device-desktop';
-    if (preg_match('@iPhone|iPod|(Android.*Chrome/[.0-9]* Mobile)@', $agent)) {
-      $device_guess = 'device-phone device';
-    } else if (preg_match('@iPad|(Android.*Chrome/)@', $agent)) {
-      $device_guess = 'device-tablet device';
+    if (phutil_nonempty_string($agent)) {
+      if (preg_match('@iPhone|iPod|(Android.*Chrome/[.0-9]* Mobile)@',
+        $agent)) {
+        $device_guess = 'device-phone device';
+      } else if (preg_match('@iPad|(Android.*Chrome/)@', $agent)) {
+        $device_guess = 'device-tablet device';
+      }
     }
 
     $classes[] = $device_guess;
 
-    if (preg_match('@Windows@', $agent)) {
-      $classes[] = 'platform-windows';
-    } else if (preg_match('@Macintosh@', $agent)) {
-      $classes[] = 'platform-mac';
-    } else if (preg_match('@X11@', $agent)) {
-      $classes[] = 'platform-linux';
+    if (phutil_nonempty_string($agent)) {
+      if (preg_match('@Windows@', $agent)) {
+        $classes[] = 'platform-windows';
+      } else if (preg_match('@Macintosh@', $agent)) {
+        $classes[] = 'platform-mac';
+      } else if (preg_match('@X11@', $agent)) {
+        $classes[] = 'platform-linux';
+      }
     }
 
     if ($this->getRequest()->getStr('__print__')) {
