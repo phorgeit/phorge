5a16b829844be9d4eb85af76c46933480682e3f6
Andre Klapper <a9016009@gmx.de>
2025-07-15 22:00:34 +0200

    Correct help for Typeahead Datasource Controls without functions
    Summary:
    Do not display an empty table of Functions and irrelevant instructions when the Typeahead Datasource Control does not support any typeahead functions in order to avoid confusion.
    
    Closes T16169
    
    Test Plan:
    * Go to http://phorge.localhost/typeahead/help/PhabricatorPeopleUserFunctionDatasource/ and still see functions listed as expected (no change)
    * Go to http://phorge.localhost/typeahead/help/ConpherenceThreadDatasource/ or http://phorge.localhost/typeahead/help/PhabricatorSearchDatasource/ and see much less text and instead information that this control does not support typeahead functions.
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T16169
    
    Differential Revision: https://we.phorge.it/D26176

--- src/applications/typeahead/controller/PhabricatorTypeaheadFunctionHelpController.php
+++ src/applications/typeahead/controller/PhabricatorTypeaheadFunctionHelpController.php
@@ -52,9 +52,6 @@ final class PhabricatorTypeaheadFunctionHelpController
 
     $title = pht('Typeahead Function Help');
 
-    $functions = $source->getAllDatasourceFunctions();
-    ksort($functions);
-
     $content = array();
 
     $content[] = '= '.pht('Overview');
@@ -72,68 +69,78 @@ final class PhabricatorTypeaheadFunctionHelpController
       'to review which functions are available for that control.',
       PhabricatorEnv::getDoclink('Search User Guide'));
 
-    $table = array();
+    $content[] = '= '.pht('Function Quick Reference');
 
-    $table_header = array(
-      pht('Function'),
-      pht('Token Name'),
-      pht('Summary'),
-    );
-    $table[] = '| '.implode(' | ', $table_header).' |';
-    $table[] = '|---|---|---|';
+    $functions = $source->getAllDatasourceFunctions();
+    if (!$functions) {
+      $content[] = pht(
+      'This control does not support typeahead functions.');
+    } else {
 
-    foreach ($functions as $function => $spec) {
-      $spec = $spec + array(
-        'summary' => null,
-        'arguments' => null,
+      ksort($functions);
+
+      $table = array();
+
+      $table_header = array(
+        pht('Function'),
+        pht('Token Name'),
+        pht('Summary'),
       );
+      $table[] = '| '.implode(' | ', $table_header).' |';
+      $table[] = '|---|---|---|';
 
-      if (idx($spec, 'arguments')) {
-        $signature = '**'.$function.'(**//'.$spec['arguments'].'//**)**';
-      } else {
-        $signature = '**'.$function.'()**';
+      foreach ($functions as $function => $spec) {
+        $spec = $spec + array(
+          'summary' => null,
+          'arguments' => null,
+        );
+
+        if (idx($spec, 'arguments')) {
+          $signature = '**'.$function.'(**//'.$spec['arguments'].'//**)**';
+        } else {
+          $signature = '**'.$function.'()**';
+        }
+
+        $name = idx($spec, 'name', '');
+        $summary = idx($spec, 'summary', '');
+
+        $table[] = '| '.$signature.' | '.$name.' | '.$summary.' |';
       }
 
-      $name = idx($spec, 'name', '');
-      $summary = idx($spec, 'summary', '');
+      $table = implode("\n", $table);
+      $content[] = pht(
+        'This table briefly describes available functions for this control. '.
+        'For details on a particular function, see the corresponding section '.
+        'below.');
+      $content[] = $table;
 
-      $table[] = '| '.$signature.' | '.$name.' | '.$summary.' |';
-    }
-
-    $table = implode("\n", $table);
-    $content[] = '= '.pht('Function Quick Reference');
-    $content[] = pht(
-      'This table briefly describes available functions for this control. '.
-      'For details on a particular function, see the corresponding section '.
-      'below.');
-    $content[] = $table;
-
-    $content[] = '= '.pht('Using Typeahead Functions');
-    $content[] = pht(
-      "In addition to typing user and project names to build queries, you can ".
-      "also type the names of special functions which give you more options ".
-      "and the ability to express more complex queries.\n\n".
-      "Functions have an internal name (like `%s`) and a human-readable name, ".
-      "like `Current Viewer`. In general, you can type either one to select ".
-      "the function. You can also click the {nav icon=search} button on any ".
-      "typeahead control to browse available functions and find this ".
-      "documentation.\n\n".
-      "This documentation uses the internal names to make it clear where ".
-      "tokens begin and end. Specifically, you will find queries written ".
-      "out like this in the documentation:\n\n%s\n\n".
-      "When this query is actually shown in the control, it will look more ".
-      "like this:\n\n%s",
-      'viewer()',
-      '> viewer(), alincoln',
-      '> {nav Current Viewer} {nav alincoln (Abraham Lincoln)}');
+      $content[] = '= '.pht('Using Typeahead Functions');
+      $content[] = pht(
+        "In addition to typing user and project names to build queries, you ".
+        "can also type the names of special functions which give you more ".
+        "options and the ability to express more complex queries.\n\n".
+        "Functions have an internal name (like `%s`) and a human-readable ".
+        "name, like `Current Viewer`. In general, you can type either one to ".
+        "select the function. You can also click the {nav icon=search} ".
+        "button on any typeahead control to browse available functions and ".
+        "find this documentation.\n\n".
+        "This documentation uses the internal names to make it clear where ".
+        "tokens begin and end. Specifically, you will find queries written ".
+        "out like this in the documentation:\n\n%s\n\n".
+        "When this query is actually shown in the control, it will look more ".
+        "like this:\n\n%s",
+        'viewer()',
+        '> viewer(), alincoln',
+        '> {nav Current Viewer} {nav alincoln (Abraham Lincoln)}');
 
 
-    $middot = "\xC2\xB7";
-    foreach ($functions as $function => $spec) {
-      $arguments = idx($spec, 'arguments', '');
-      $name = idx($spec, 'name');
-      $content[] = '= '.$function.'('.$arguments.') '.$middot.' '.$name;
-      $content[] = $spec['description'];
+      $middot = "\xC2\xB7";
+      foreach ($functions as $function => $spec) {
+        $arguments = idx($spec, 'arguments', '');
+        $name = idx($spec, 'name');
+        $content[] = '= '.$function.'('.$arguments.') '.$middot.' '.$name;
+        $content[] = $spec['description'];
+      }
     }
 
     $content = implode("\n\n", $content);
