674ef80b17b9a37bd6351dd019f7cf768b075a03
Andre Klapper <a9016009@gmx.de>
2025-08-12 16:37:34 +0200

    Docs: Replace your.install.com with phorge.example.com
    Summary:
    Numerous other docs also use example.com, and that domain is intentionally meant for such purposes.
    Makes grep results more complete for the next project renaming.
    
    Test Plan: None.
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: mainframe98, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26254

--- src/applications/transactions/view/PhabricatorApplicationEditHTTPParameterHelpView.php
+++ src/applications/transactions/view/PhabricatorApplicationEditHTTPParameterHelpView.php
@@ -34,7 +34,7 @@ final class PhabricatorApplicationEditHTTPParameterHelpView
     $object = $this->getObject();
     $fields = $this->getFields();
 
-    $uri = 'https://your.install.com/application/edit/';
+    $uri = 'https://phorge.example.com/application/edit/';
 
     // Remove fields which do not expose an HTTP parameter type.
     $types = array();
@@ -67,10 +67,10 @@ browser's address bar. It will usually look something like this:
 %s
 ```
 
-However, `your.install.com` will be the domain where your copy of this software
-is installed, and `application/` will be the URI for an application. Some
-applications have multiple forms for creating objects or URIs that look a little
-different than this example, so the URI may not look exactly like this.
+However, `phorge.example.com` will be the domain where your copy of this
+software is installed, and `application/` will be the URI for an application.
+Some applications have multiple forms for creating objects or URIs that look a
+little different than this example, so the URI may not look exactly like this.
 
 To prefill the form, add properly encoded HTTP parameters to the URI. You
 should end up with something like this:
