de54f8c91e273e2e986e01797e4e75265dee876c
Valerio Bozzolan <nsa+gitspam@succhia.cz>
2025-09-05 10:37:03 +0200

    lipsum revisions: fix missing 'created this revision' (adopt transactions)
    Summary:
    When generating test revisions, the 'created this revision' is not missing anymore,
    and the nonsense 'updated this revision to Diff 123' is not shown anymore,
    since this is a creation, and not an update.
    
    Basically we avoid to manually call setters, and we use transactions instead.
    
    Closes T16232
    
    Test Plan:
        ./bin/lipsum generate revisions
    
    Enjoy the new 'created this revision'.
    
    You also don't see anymore the nonsense 'updated this revision'.
    
    Enjoy the fake revisions that are now more similar to the real ones.
    
    Reviewers: O1 Blessed Committers, aklapper
    
    Reviewed By: O1 Blessed Committers, aklapper
    
    Subscribers: aklapper, tobiaswiese, Matthew, Cigaryno
    
    Maniphest Tasks: T16232
    
    Differential Revision: https://we.phorge.it/D26286

--- src/applications/differential/lipsum/PhabricatorDifferentialRevisionTestDataGenerator.php
+++ src/applications/differential/lipsum/PhabricatorDifferentialRevisionTestDataGenerator.php
@@ -13,22 +13,35 @@ final class PhabricatorDifferentialRevisionTestDataGenerator
     $author = $this->loadPhabricatorUser();
 
     $revision = DifferentialRevision::initializeNewRevision($author);
-    $revision->attachReviewers(array());
-    $revision->attachActiveDiff(null);
-
-    // This could be a bit richer and more formal than it is.
-    $revision->setTitle($this->generateTitle());
-    $revision->setSummary($this->generateDescription());
-    $revision->setTestPlan($this->generateDescription());
 
     $diff = $this->generateDiff($author);
+    $type_create = PhabricatorTransactions::TYPE_CREATE;
     $type_update = DifferentialRevisionUpdateTransaction::TRANSACTIONTYPE;
+    $type_title = DifferentialRevisionTitleTransaction::TRANSACTIONTYPE;
+    $type_summary = DifferentialRevisionSummaryTransaction::TRANSACTIONTYPE;
+    $type_testplan = DifferentialRevisionTestPlanTransaction::TRANSACTIONTYPE;
 
+    // Create associative array of transaction types and their new value.
+    $changes = array();
+    $changes[$type_create] = null; // Value not needed here.
+
+    // This could be a bit richer and more formal than it is.
+    $changes[$type_update] = $diff->getPHID();
+    $changes[$type_title] = $this->generateTitle();
+    $changes[$type_summary] = $this->generateDescription();
+    $changes[$type_testplan] = $this->generateDescription();
+
+    // Create transactions.
     $xactions = array();
+    foreach ($changes as $type => $new_value) {
+      $xaction = new DifferentialTransaction();
+      $xaction->setTransactionType($type);
+      if ($new_value !== null) {
+        $xaction->setNewValue($new_value);
+      }
 
-    $xactions[] = id(new DifferentialTransaction())
-      ->setTransactionType($type_update)
-      ->setNewValue($diff->getPHID());
+      $xactions[] = $xaction;
+    }
 
     id(new DifferentialTransactionEditor())
       ->setActor($author)
