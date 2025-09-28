phorgeit/phorge

commit ce3c11dc8685f6351f87545f9270f76686f90f5a

UIExamples: Set the sidebar as page navigation
Summary:
This makes it behave like a regular sidebar with regards to the footer,
so that it doesn't result in the footer filling the width of the screen.

Ref T16188

Test Plan:
* Enable prototype applications
* Visit http://phorge.localhost/uiexample/view/PHUITimelineExample/ and look at the footer

Reviewers: O1 Blessed Committers, aklapper

Reviewed By: O1 Blessed Committers, aklapper

Subscribers: aklapper, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Maniphest Tasks: T16188

Differential Revision: https://we.phorge.it/D26209

src/applications/uiexample/controller/PhabricatorUIExampleRenderController.php
@@ -57,7 +57,7 @@ final class PhabricatorUIExampleRenderController extends PhabricatorController {
 
     return $this->newPage()
       ->setTitle($example->getName())
-      ->appendChild($nav);
+      ->setNavigation($nav);
   }
 
 }

commit 434e272fbf8e6cecd8824681d1c114e565f2f3d2

UIExamples: Fix missing styling
Summary:
This is a regression from ce3c11dc8685.
Previously, the whole page content was part of the navigation,
which itself was set to be a child of the page container.
This caused several styling issues and was fixed. However, the
page content was still a child of the navigation and navigation
doesn't render included styling.

Closes T16283

Test Plan:
* Look at http://phorge.localhost/uiexample/view/PHUIBadgeExample/
* Look at http://phorge.localhost/uiexample/view/PHUILeftRightExample/

Reviewers: O1 Blessed Committers, aklapper

Reviewed By: O1 Blessed Committers, aklapper

Subscribers: aklapper, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno

Maniphest Tasks: T16283

Differential Revision: https://we.phorge.it/D26389

src/applications/uiexample/controller/PhabricatorUIExampleRenderController.php
@@ -48,16 +48,15 @@ final class PhabricatorUIExampleRenderController extends PhabricatorController {
       ->appendChild($example->getDescription())
       ->setSeverity(PHUIInfoView::SEVERITY_NODATA);
 
-    $nav->appendChild(
-      array(
-        $crumbs,
-        $note,
-        $result,
-      ));
-
     return $this->newPage()
       ->setTitle($example->getName())
-      ->setNavigation($nav);
+      ->setNavigation($nav)
+      ->appendChild(
+        array(
+          $crumbs,
+          $note,
+          $result,
+        ));
   }
 
 }