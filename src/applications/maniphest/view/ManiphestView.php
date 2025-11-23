0706fd1568d2c0feb9d9831c13356a44bfe64fc1
Andre Klapper <a9016009@gmx.de>
2025-11-23 12:59:39 +0100

    Remove unused function ManiphestView::renderTagForTask()
    Summary: Its only usage was removed in rP0d77a7f3.
    
    Test Plan: Grep the codebase.
    
    Reviewers: O1 Blessed Committers, avivey
    
    Reviewed By: O1 Blessed Committers, avivey
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26515

--- src/applications/maniphest/view/ManiphestView.php
+++ src/applications/maniphest/view/ManiphestView.php
@@ -1,14 +1,3 @@
 <?php
 
-abstract class ManiphestView extends AphrontView {
-
-  public static function renderTagForTask(ManiphestTask $task) {
-    $status = $task->getStatus();
-    $status_name = ManiphestTaskStatus::getTaskStatusFullName($status);
-
-    return id(new PHUITagView())
-        ->setType(PHUITagView::TYPE_STATE)
-        ->setName($status_name);
-  }
-
-}
+abstract class ManiphestView extends AphrontView {}
