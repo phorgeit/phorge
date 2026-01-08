fb37870bf133d7f585d0a8bf44b9f921a256bd51
Andre Klapper <a9016009@gmx.de>
2026-01-08 10:33:29 +0100

    Explicitly set Arcanist path for @include_once in scripts/init/lib.php
    Summary: Do the same $root dance as already existing in PhabricatorStartup.php to avoid a static code analysis error.
    
    Test Plan:
    Run PhpStan, do not get the following output anymore:
    `/var/www/html/phorge/phorge/scripts/init/lib.php:12: Path in include_once() "arcanist/support/init/init-script.php" is not a file or it does not exist.`
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: mainframe98, tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26627

--- scripts/init/lib.php
+++ scripts/init/lib.php
@@ -4,12 +4,17 @@ function init_phabricator_script(array $options) {
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
 
+  $root = null;
+  if (!empty($_SERVER['PHUTIL_LIBRARY_ROOT'])) {
+    $root = $_SERVER['PHUTIL_LIBRARY_ROOT'];
+  }
+
   $include_path = ini_get('include_path');
   ini_set(
     'include_path',
     $include_path.PATH_SEPARATOR.dirname(__FILE__).'/../../../');
 
-  $ok = @include_once 'arcanist/support/init/init-script.php';
+  $ok = @include_once $root.'arcanist/support/init/init-script.php';
   if (!$ok) {
     echo
       'FATAL ERROR: Unable to load the "Arcanist" library. '.
