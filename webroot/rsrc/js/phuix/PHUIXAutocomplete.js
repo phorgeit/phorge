a129563ba71962535fa6ebe43823edc8a352f8a3
Valerio Bozzolan <nsa+gitspam@succhia.cz>
2026-01-23 19:43:19 +0100

    Autocomplete: minor refactor
    Summary:
    Do the same in less code.
    
    Implement this small tip:
    
    https://we.phorge.it/D26681#43044
    
    Ref T16334
    
    Test Plan:
    Same test plan as D26681.
    
    Share a comment in Maniphest Task pinging somebody. Still works.
    Do the same with somebody subscribed. Still works and the subscribed has priority.
    
    Share a comment in Differential Diff (which does not support this feature) pinging somebody.
    Still works.
    
    Reviewers: O1 Blessed Committers, aklapper
    
    Reviewed By: O1 Blessed Committers, aklapper
    
    Subscribers: aklapper, tobiaswiese, Matthew, Cigaryno
    
    Maniphest Tasks: T16334
    
    Differential Revision: https://we.phorge.it/D26705

--- webroot/rsrc/js/phuix/PHUIXAutocomplete.js
+++ webroot/rsrc/js/phuix/PHUIXAutocomplete.js
@@ -157,9 +157,9 @@ JX.install('PHUIXAutocomplete', {
           JX.bind(this, this._onresults, code));
 
         datasource.setTransformer(JX.bind(this, this._transformresult));
-        // List users first who are involved in the object
-        var involvedUsers = 'involvedUsers' in spec ? spec.involvedUsers : null;
-        if (involvedUsers !== null && involvedUsers.length !== 0) {
+        // List users first who are involved in the object.
+        var involvedUsers = spec.involvedUsers;
+        if (involvedUsers && involvedUsers.length !== 0) {
           datasource.setSortHandler(function(value, list) {
             list.sort(function(u,v){
             var usort = involvedUsers.includes(u.id) ? 0 : 1;
