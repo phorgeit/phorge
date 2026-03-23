066c1b124a7321734ce8a1cd9a890b02df9d3190
Andre Klapper <a9016009@gmx.de>
2026-03-23 21:02:33 +0100

    Conduit: Add basic input validation to macro.query
    Summary: Check at least if input is an array when an array is expected and return a proper Conduit error instead of a cryptic exception.
    
    Test Plan: Go to http://phorge.localhost/conduit/method/macro.query/ and add random values in fields
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Differential Revision: https://we.phorge.it/D26874

--- src/applications/macro/conduit/MacroQueryConduitAPIMethod.php
+++ src/applications/macro/conduit/MacroQueryConduitAPIMethod.php
@@ -24,6 +24,15 @@ final class MacroQueryConduitAPIMethod extends MacroConduitAPIMethod {
     return 'list<dict>';
   }
 
+  protected function defineErrorTypes() {
+    return array(
+      'ERR-BAD-AUTHORPHIDS' => pht('Author PHIDs must be a list.'),
+      'ERR-BAD-PHIDS' => pht('PHIDs must be a list.'),
+      'ERR-BAD-IDS' => pht('IDs must be a list.'),
+      'ERR-BAD-NAMES' => pht('Names must be a list.'),
+    );
+  }
+
   protected function execute(ConduitAPIRequest $request) {
     $query = id(new PhabricatorMacroQuery())
       ->setViewer($request->getUser())
@@ -36,14 +45,23 @@ final class MacroQueryConduitAPIMethod extends MacroConduitAPIMethod {
     $names = $request->getValue('names');
 
     if ($author_phids) {
+      if (!is_array($author_phids)) {
+        throw new ConduitException('ERR-BAD-AUTHORPHIDS');
+      }
       $query->withAuthorPHIDs($author_phids);
     }
 
     if ($phids) {
+      if (!is_array($phids)) {
+        throw new ConduitException('ERR-BAD-PHIDS');
+      }
       $query->withPHIDs($phids);
     }
 
     if ($ids) {
+      if (!is_array($ids)) {
+        throw new ConduitException('ERR-BAD-IDS');
+      }
       $query->withIDs($ids);
     }
 
@@ -52,6 +70,9 @@ final class MacroQueryConduitAPIMethod extends MacroConduitAPIMethod {
     }
 
     if ($names) {
+      if (!is_array($names)) {
+        throw new ConduitException('ERR-BAD-NAMES');
+      }
       $query->withNames($names);
     }
 
