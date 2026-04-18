9f90e22847668500adfaeb8e66148c211f09251f
Andre Klapper <a9016009@gmx.de>
2026-04-18 10:54:17 +0200

    Deprecate Conduit's differential.createcomment endpoint
    Summary:
    `differential.revision.edit` covers everything that `differential.createcomment` does.
    * All values for `action` listed in DifferentialCreateCommentConduitAPIMethod are also available as `transactions` in differential.revision.edit
    * `message` in `differential.createcomment` equals `comment` in `differential.revision.edit`
    * `silent` and `attach_inlines` are ignored and have no effect anyway - see the code comments in DifferentialCreateCommentConduitAPIMethod
    * Output is in both cases either a success or a fail, nothing else of interest gets returned which would require comparing feature parity
    
    Closes T16606
    
    Test Plan:
    * Have an existing revision in Differential
    * Go to http://phorge.localhost/conduit/method/differential.createcomment/ and add a comment to that revision
    * Go to http://phorge.localhost/conduit/method/differential.revision.edit/ and add a comment to that revision
    * Go to http://phorge.localhost/conduit/method/differential.createcomment/ and look at the top box
    
    Reviewers: O1 Blessed Committers, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, valerio.bozzolan
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T16606
    
    Differential Revision: https://we.phorge.it/D26928

--- src/applications/differential/conduit/DifferentialCreateCommentConduitAPIMethod.php
+++ src/applications/differential/conduit/DifferentialCreateCommentConduitAPIMethod.php
@@ -12,13 +12,14 @@ final class DifferentialCreateCommentConduitAPIMethod
   }
 
   public function getMethodStatus() {
-    return self::METHOD_STATUS_FROZEN;
+    return self::METHOD_STATUS_DEPRECATED;
   }
 
   public function getMethodStatusDescription() {
     return pht(
-      'This method is frozen and will eventually be deprecated. New code '.
-      'should use "differential.revision.edit" instead.');
+      'This method has been deprecated since %s in favor of %s.',
+      '04/2026',
+      'differential.revision.edit');
   }
 
   protected function defineParamTypes() {
