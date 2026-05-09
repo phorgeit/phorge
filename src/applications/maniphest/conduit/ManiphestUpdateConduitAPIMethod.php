9d7b859db638f6a1973f62c36d023c99028784f2
Andre Klapper <a9016009@gmx.de>
2026-05-09 14:14:22 +0200

    Conduit: Deprecate maniphest.update endpoint
    Summary:
    Superseded by `maniphest.edit` which offers feature parity in combination with `maniphest.priority.search` and `maniphest.search`.
    
    Closes T16633
    
    Test Plan:
    Go to http://phorge.localhost/conduit/method/maniphest.update/ and look at the top box.
    See also T16633 for more details.
    
    Reviewers: O1 Blessed Committers, mainframe98
    
    Reviewed By: O1 Blessed Committers, mainframe98
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T16633
    
    Differential Revision: https://we.phorge.it/D26970

--- src/applications/maniphest/conduit/ManiphestUpdateConduitAPIMethod.php
+++ src/applications/maniphest/conduit/ManiphestUpdateConduitAPIMethod.php
@@ -11,13 +11,14 @@ final class ManiphestUpdateConduitAPIMethod extends ManiphestConduitAPIMethod {
   }
 
   public function getMethodStatus() {
-    return self::METHOD_STATUS_FROZEN;
+    return self::METHOD_STATUS_DEPRECATED;
   }
 
   public function getMethodStatusDescription() {
     return pht(
-      'This method is frozen and will eventually be deprecated. New code '.
-      'should use "maniphest.edit" instead.');
+      'This method has been deprecated since %s in favor of %s.',
+      '05/2026',
+      'maniphest.edit');
   }
 
   protected function defineErrorTypes() {
