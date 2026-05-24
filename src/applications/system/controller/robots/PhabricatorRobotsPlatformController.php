d5e8cd1c75305dc0e813ccd29d5b79237679c8a5
Aviv Eyal <avivey@gmail.com>
2026-05-24 16:16:42 +0300

    Make robots.txt extensible
    Summary:
    Allow applications/extensions to add their own elements to robots.txt, thus preventing the good robots from getting lost. Evil robots are still a problem.
    
    Close T16121
    
    Test Plan: curl `/robots.txt` before and after, compare.
    
    Reviewers: O1 Blessed Committers, mainframe98, valerio.bozzolan
    
    Reviewed By: O1 Blessed Committers, mainframe98, valerio.bozzolan
    
    Subscribers: tobiaswiese, valerio.bozzolan, Matthew, Cigaryno
    
    Maniphest Tasks: T16121
    
    Differential Revision: https://we.phorge.it/D27013

--- src/applications/system/controller/robots/PhabricatorRobotsPlatformController.php
+++ src/applications/system/controller/robots/PhabricatorRobotsPlatformController.php
@@ -4,38 +4,27 @@ final class PhabricatorRobotsPlatformController
   extends PhabricatorRobotsController {
 
   protected function newRobotsRules() {
-    $out = array();
+    $parts = array();
 
-    // Prevent indexing of '/diffusion/', since the content is not generally
-    // useful to index, web spiders get stuck scraping the history of every
-    // file, and much of the content is Ajaxed in anyway so spiders won't even
-    // see it. These pages are also relatively expensive to generate.
+    $parts[] = array(
+      'User-Agent: *',
+    );
 
-    // Note that this still allows commits (at '/rPxxxxx') to be indexed.
-    // They're probably not hugely useful, but suffer fewer of the problems
-    // Diffusion suffers and are hard to omit with 'robots.txt'.
-
-    $out[] = 'User-Agent: *';
-    $out[] = 'Disallow: /diffusion/';
-    $out[] = 'Disallow: /source/';
-    // See T15670. Also prevent directly accessing commits in Diffusion.
-    $out[] = 'Disallow: /r*';
-
-    // See T15662. Prevent indexing line anchor links in Pastes. Per RFC 9309
-    // section 2.2.3, percentage-encode "$" to avoid interpretation as end of
-    // match pattern. However, crawlers may not abide by it but follow the
-    // original standard at https://www.robotstxt.org/orig.html with no mention
-    // how to interpret characters like "$" and thus entirely ignore this rule.
-    $out[] = 'Disallow: /P*%24*';
+    $extensions = PhorgeRobotsTxtEngine::getAllExtensions();
+    foreach ($extensions as $extension) {
+      $parts[] = $extension->getRules();
+    }
 
     // Add a small crawl delay (number of seconds between requests) for spiders
     // which respect it. The intent here is to prevent spiders from affecting
     // performance for users. The possible cost is slower indexing, but that
     // seems like a reasonable tradeoff, since most Phabricator installs are
     // probably not hugely concerned about cutting-edge SEO.
-    $out[] = 'Crawl-delay: 1';
+    $parts[] = array(
+      'Crawl-delay: 1',
+    );
 
-    return $out;
+    return array_mergev($parts);
   }
 
 }
